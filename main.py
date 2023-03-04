e7ae917b00595321be84477630642c8f8678755a
Erick Mercado <erick911@gmail.com>
2023-03-03 21:40:55 -0300

    cambios para desplegar web

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from flask import Flask, render_template
 
-
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-  return 'hola'
+    return render_template('index.html')
 
 
-app.run(host='0.0.0.0')
+
+
+app.run(host='0.0.0.0', port=3000, debug=True)
